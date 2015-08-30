3fb066fe391703cbfdd4a67e52c8a319f68fa2d2
malev <marcosvanetta@gmail.com>
2015-08-30 00:04:24 -0500

    Build HTML files

--- gulpfile.js
+++ gulpfile.js
@@ -7,10 +7,9 @@ var browserify = require('browserify');
 var watchify = require('watchify');
 var babel = require('babelify');
 var sass = require('gulp-sass');
-var nunjucks = require('gulp-nunjucks');
 
 function compile(watch) {
-  var bundler = watchify(browserify('./src/index.js', { debug: true }).transform(babel));
+  var bundler = watchify(browserify('./src/js/app.js', { debug: true }).transform(babel));
 
   function rebundle() {
     bundler.bundle()
@@ -19,6 +18,11 @@ function compile(watch) {
       .pipe(buffer())
       .pipe(sourcemaps.init({ loadMaps: true }))
       .pipe(sourcemaps.write('./'))
+      .pipe(gulp.dest('./build/js'));
+    gulp.src('./src/sass/**/*.scss')
+      .pipe(sass().on('error', sass.logError))
+      .pipe(gulp.dest('./build/css'));
+    gulp.src('./src/index.html')
       .pipe(gulp.dest('./build'));
   }
 
@@ -32,26 +36,19 @@ function compile(watch) {
   rebundle();
 }
 
+function watch() {
+  return compile(true);
+};
+
 gulp.task('sass', function () {
   gulp.src('./src/sass/**/*.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest('./build/css'));
 });
 
-function watch() {
-  return compile(true);
-};
-
-gulp.task('sass', function () {
-  gulp.src('./src/css/**/*.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./build/css'));
-});
-
 gulp.task('html', function () {
-    return gulp.src('./src/**/*.html')
-        .pipe(nunjucks())
-        .pipe(gulp.dest('./build'));
+  gulp.src('./src/index.html')
+    .pipe(gulp.dest('./build'));
 });
 
 gulp.task('browser-sync', function() {
@@ -61,6 +58,7 @@ gulp.task('browser-sync', function() {
         }
     });
 });
+
 gulp.task('build', function() { return compile(); });
 gulp.task('watch', function() { return watch(); });
-gulp.task('default', ['html', 'sass']);
+gulp.task('default', ['html', 'sass', 'build']);
